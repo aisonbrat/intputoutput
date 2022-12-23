f7832d535958af425b44d2e33dfbb1b461c8970c
Aisultan Zhaksytayev <zhaksytayev@gmail.com>
2022-12-23 14:45:04 +0600

    first

--- inputOutputScrollPos/ViewController.swift
+++ inputOutputScrollPos/ViewController.swift
@@ -1,19 +1,57 @@
 //
 //  ViewController.swift
-//  inputOutputScrollPos
+//  InputOutput
 //
-//  Created by Aisultan Zhaksytayev on 23.12.2022.
+//  Created by Aisultan Zhaksytayev on 20.12.2022.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+
+    
+    
+
+    @IBOutlet weak var titleText: UITextField!
+    @IBOutlet weak var descriptionText: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var photo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//
+//        titleText.delegate = self
+//        descriptionText.delegate = self
+        submitButtonOutlet.layer.cornerRadius = 18
     }
-
-
+    @IBOutlet weak var submitButtonOutlet: UIButton!
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    @IBAction func submitButton(_ sender: Any) {
+        let titleTextO = titleText.text
+        let descTextO = descriptionText.text
+//        textView.text = textViewO
+//        textView.text = descriptionText.text
+        textView.text = "Title: \(titleText.text!)\nDescription: \(descriptionText.text!)"
+//        textView.text = "Title: \(titleText.text!)\nDescription: \(descriptionText.text!)"
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        titleText.resignFirstResponder()
+    }
 }
+
+extension ViewController : UITextFieldDelegate {
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
 
